7b8f8c3db175408c7d3c80cf98d84588f15101b2
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2025-10-05 07:03:27 +0000

    Configuration Json Mobile

---
+++ src/Controller/ConfigController.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/configurations')]
+class ConfigController extends AbstractController
+{
+    #[Route('/android_v1.json', name: 'app_android_config', methods: ['GET'])]
+    public function androidV1(): JsonResponse
+    {
+        // Contenu du JSON (doit correspondre à la version locale)
+        $config = [
+            'settings' => [
+                'screenshots_enabled' => true,
+            ],
+            'rules' => [
+                [
+                    'patterns' => ['.*'],
+                    'properties' => [
+                        'context' => 'default',
+                        'uri' => 'hotwire://fragment/web',
+                        'pull_to_refresh_enabled' => true,
+                    ],
+                ],
+                [
+                    'patterns' => ['/login$', '/register$', '/new$', '/edit$'],
+                    'properties' => [
+                        'context' => 'modal',
+                        'uri' => 'hotwire://fragment/web/modal/sheet',
+                        'pull_to_refresh_enabled' => false,
+                    ],
+                ],
+            ],
+        ];
+
+        return new JsonResponse($config);
+    }
+}
