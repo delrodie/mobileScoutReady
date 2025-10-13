25020f2a9d6ee6d8824be8684959f8605c773d13
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2025-10-13 09:21:56 +0000

    Gestion de l'inscription

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -22,10 +22,14 @@ class HomeController extends AbstractController
     #[Route('/phone', name:'app_search_phone', methods: ['GET','POST'])]
     public function phone(Request $request, ScoutRepository $scoutRepository)
     {
+        $session = $request->getSession();
 
         if ($this->isCsrfTokenValid('_searchPhone', $request->get('_csrf_token'))){
             $phoneRequest = $request->request->get('_phone_search');
             $scout = $scoutRepository->findOneBy(['telephone' => $phoneRequest]);
+
+            // Mise en session du numero de telephone
+            $session->set('_phone_input', $phoneRequest);
 
             if (!$scout){
                 if ($request->headers->has('Turbo-Frame')){
--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -12,8 +13,16 @@ use Symfony\Component\Routing\Attribute\Route;
 class InscriptionController extends AbstractController
 {
     #[Route('/', name:'app_inscription_civile')]
-    public function civile(): Response
+    public function civile(Request $request): Response
     {
-        return $this->render('default/inscription_cvile.html.twig');
+        return $this->render('scout/inscription_civile.html.twig',[
+            'phone' => $request->getSession()->get('_phone_input') ?? null,
+        ]);
+    }
+
+    #[Route('/scoute', name:'app_inscription_scoute')]
+    public function scoute(Request $request): Response
+    {
+
     }
 }
