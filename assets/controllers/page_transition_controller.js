03b82407b30ac84fb84ebc513c3bc7e61e542be5
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2025-11-03 03:58:46 +0000

    transition

--- assets/controllers/page_transition_controller.js
+++ assets/controllers/page_transition_controller.js
@@ -2,22 +2,39 @@ import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
     connect() {
-        document.addEventListener('turbo:before-visit', this.beforeVisit);
+        document.addEventListener('turbo:before-fetch-request', this.beforeFetch);
+        document.addEventListener('turbo:before-render', this.beforeRender);
         document.addEventListener('turbo:render', this.onRender);
+
+        document.body.classList.remove('fade-out');
+        document.body.classList.add('fade-in');
         console.log('Controleur de transition de page activé.');
-        document.body.classList.remove('turbo-transition');
     }
 
     disconnect() {
-        document.removeEventListener('turbo:before-visit', this.beforeVisit);
-        document.removeEventListener('turbo:render', this.onRender);
+        document.removeEventListener('turbo:before-fetch-request', this.beforeFetch);
+        document.removeEventListener('turbo:before-render', this.beforeRender);
+        document.removeEventListener('turbo-render', this.onRender);
     }
 
-    beforeVisit = () => {
-        document.body.classList.add('turbo-transition');
+    beforeFetch = () => {
+        document.body.classList.add('fade-out');
+        document.body.classList.remove('fade-in');
+    }
+
+    beforeRender = (event) => {
+        const newBody = event.detail.newBody;
+
+        newBody.classList.add('fade-out');
+        newBody.classList.remove('fade-in');
+
+        document.body.classList.remove('fade-out');
     }
 
     onRender = () => {
-        document.body.classList.remove('turbo-transition');
+        setTimeout(() => {
+            document.body.classList.remove('fade-out');
+            document.body.classList.add('fade-in');
+        }, 10);
     }
 }
